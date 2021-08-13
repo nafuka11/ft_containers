43b8b424be879d08dc51195e7efa32f32116dfe3
nfukada <nfukada@student.42tokyo.jp>
2021-08-13 23:09:39 +0900

    feat(type_traits): add enable_if

--- includes/type_traits.hpp
+++ includes/type_traits.hpp
@@ -14,6 +14,16 @@ namespace ft
         typedef typename Iterator::iterator_category iterator_category;
     };
 
+    // enable_if
+    template <bool B, class T = void>
+    struct enable_if {};
+
+    template <class T>
+    struct enable_if<true, T>
+    {
+        typedef T type;
+    };
+
 } /* namespace ft */
 
 #endif /* TYPE_TRAITS_HPP */

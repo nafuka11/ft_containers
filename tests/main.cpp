313500c5ba6c9b4fc33cacddc74c104505f397a6
nfukada <nfukada@student.42tokyo.jp>
2021-08-16 15:50:29 +0900

    feat: add main.cpp

---
+++ tests/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#if 0 //CREATE A REAL STL EXAMPLE
+// #include <map>
+// #include <stack>
+#include <vector>
+namespace ft = std;
+#else
+// #include <map.hpp>
+// #include <stack.hpp>
+#include <vector.hpp>
+#endif
+
+int main()
+{
+}

fdca3466f59d2628b1e695a886902f0369ed0166
nfukada <nfukada@student.42tokyo.jp>
2021-08-21 13:46:28 +0900

    feat: implement vector class that compiles

--- includes/vector.hpp
+++ includes/vector.hpp
@@ -40,6 +40,92 @@ namespace ft
             pointer ptr_;
     };
 
+    template <class T, class Allocator = std::allocator<T> >
+    class vector
+    {
+        public:
+            // Member types
+            typedef T                                        value_type;
+            typedef Allocator                                allocator_type;
+            typedef typename allocator_type::reference       reference;
+            typedef typename allocator_type::const_reference const_reference;
+            typedef ft::vector_iterator_<T>                  iterator;
+            //typedef implementation-defined                   const_iterator;
+            typedef typename allocator_type::size_type       size_type;
+            typedef typename allocator_type::difference_type difference_type;
+            typedef typename allocator_type::pointer         pointer;
+            typedef typename allocator_type::const_pointer   const_pointer;
+            //typedef std::reverse_iterator<iterator>          reverse_iterator;
+            //typedef std::reverse_iterator<const_iterator>    const_reverse_iterator;
+
+            // Member functions
+            explicit vector(const allocator_type& alloc = allocator_type()) : alloc_(alloc) {}
+            explicit vector(size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type()) : alloc_(alloc)
+            {
+                allocate(n);
+                construct_at_end(n, val);
+            }
+            template <class InputIterator>
+            vector(typename ft::enable_if<!ft::is_integral<InputIterator>::value>::type first,
+                   InputIterator last,
+                   const allocator_type& alloc = allocator_type()) : alloc_(alloc)
+            {
+                (void)first;
+                (void)last;
+            }
+            vector(const vector& x)
+            {
+                (void)x;
+            }
+            // Member functions: Iterators
+            iterator begin()
+            {
+                return iterator(first_);
+            }
+            // const_iterator begin() const;
+            iterator end()
+            {
+                return iterator(last_);
+            }
+            // const_iterator end() const;
+            size_type size() const
+            {
+                return last_ - first_;
+            }
+            // Member functions: Capacity
+            // Member functions: Element access
+            reference operator[] (size_type n)
+            {
+                return first_[n];
+            }
+            const_reference operator[] (size_type n) const
+            {
+                return first_[n];
+            }
+            // Member functions: Modifiers
+            // Member functions: Allocator
+            // Non-member functions
+        private:
+            allocator_type alloc_;
+            pointer first_;
+            pointer last_;
+            pointer capacity_end_;
+
+            void allocate(size_type size)
+            {
+                first_ = alloc_.allocate(size);
+            }
+
+            void construct_at_end(size_type size, const_reference value)
+            {
+                for (size_type i = 0; i < size; i++)
+                {
+                    alloc_.construct(first_ + i, value);
+                }
+                last_ = first_ + size;
+                capacity_end_ = last_;
+            }
+    };
 
 } /* namespace ft */
 

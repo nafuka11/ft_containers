b7e9ba61753d8092cf3a746899132ad1d15afb2d
nfukada <nfukada@student.42tokyo.jp>
2021-08-25 01:04:18 +0900

    feat(vector): implement const begin/end, rbegin/rend

--- includes/vector.hpp
+++ includes/vector.hpp
@@ -201,12 +201,34 @@ namespace ft
             {
                 return iterator(first_);
             }
-            // const_iterator begin() const;
+            const_iterator begin() const
+            {
+                return const_iterator(first_);
+            }
             iterator end()
             {
                 return iterator(last_);
             }
-            // const_iterator end() const;
+            const_iterator end() const
+            {
+                return const_iterator(last_);
+            }
+            reverse_iterator rbegin()
+            {
+                return reverse_iterator(end());
+            }
+            const_reverse_iterator rbegin() const
+            {
+                return const_reverse_iterator(end());
+            }
+            reverse_iterator rend()
+            {
+                return reverse_iterator(begin());
+            }
+            const_reverse_iterator rend() const
+            {
+                return const_reverse_iterator(begin());
+            }
             size_type size() const
             {
                 return last_ - first_;

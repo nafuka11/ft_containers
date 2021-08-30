a044f0c2254d35d8f8e086f75eff3cd9d7bdad77
nfukada <nfukada@student.42tokyo.jp>
2021-08-30 14:18:10 +0900

    feat(algorithm): implement lexicographical_compare()

--- includes/algorithm.hpp
+++ includes/algorithm.hpp
@@ -7,14 +7,12 @@ namespace ft
     bool equal(InputIterator1 first1, InputIterator1 last1,
                InputIterator2 first2)
     {
-        while (first1 != last1)
+        for (; first1 != last1; ++first1, ++first2)
         {
             if (!(*first1 == *first2))
             {
                 return false;
             }
-            ++first1;
-            ++first2;
         }
         return true;
     }
@@ -22,16 +20,50 @@ namespace ft
     bool equal(InputIterator1 first1, InputIterator1 last1,
                InputIterator2 first2, BinaryPredicate pred)
     {
-        while (first1 != last1)
+        for (; first1 != last1; ++first1, ++first2)
         {
             if (!pred(*first1 == *first2))
             {
                 return false;
             }
-            ++first1;
-            ++first2;
         }
         return true;
+    }
+
+    template <class InputIterator1, class InputIterator2>
+    bool lexicographical_compare(InputIterator1 first1, InputIterator1 last1,
+                                 InputIterator2 first2, InputIterator2 last2)
+    {
+        for (; (first1 != last1) && (first2 != last2); ++first1, ++first2)
+        {
+            if (*first1 < *first2)
+            {
+                return true;
+            }
+            if (*first1 > *first2)
+            {
+                return false;
+            }
+        }
+        return (first1 == last1) && (first2 != last2);
+    }
+    template <class InputIterator1, class InputIterator2, class Compare>
+    bool lexicographical_compare(InputIterator1 first1, InputIterator1 last1,
+                                 InputIterator2 first2, InputIterator2 last2,
+                                 Compare comp)
+    {
+        for (; (first1 != last1) && (first2 != last2); ++first1, ++first2)
+        {
+            if (comp(*first1, *first2))
+            {
+                return true;
+            }
+            if (comp(*first2, *first1))
+            {
+                return false;
+            }
+        }
+        return (first1 == last1) && (first2 != last2);
     }
 
 } /* namespace ft */

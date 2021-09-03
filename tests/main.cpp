e164364b53f1de132efc77e6f28b45fd0aa873c6
nfukada <nfukada@student.42tokyo.jp>
2021-09-03 12:30:47 +0900

    test(map): test pair class

--- tests/main.cpp
+++ tests/main.cpp
@@ -3,4 +3,5 @@
 int main()
 {
     test_vector();
+    test_map();
 }
---
+++ tests/map/PairTester.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include "map/PairTester.hpp"
+
+PairTester::PairTester() {}
+
+PairTester::~PairTester() {}
+
+void PairTester::test_all()
+{
+    print_test_case("pair");
+
+    test_constructor();
+    test_assignment_operator();
+    test_relational_operator();
+}
+
+void PairTester::test_constructor()
+{
+    ft::pair<int, int> p1;
+    std::cout << "pair<int, int>(): " << p1 << std::endl;
+
+    ft::pair<int, int> p2(1, 2);
+    std::cout << "pair<int, int>(1, 2): " << p2 << std::endl;
+
+    ft::pair<int, int> p3(1, 2);
+    std::cout << "pair<int, int>(pair(1, 2)): " << p3 << std::endl;
+}
+
+void PairTester::test_assignment_operator()
+{
+    ft::pair<int, int> p1(12, 34);
+    ft::pair<int, int> p2(56, 78);
+    p2 = p1;
+    std::cout << "pair<int, int> p = " << p1 << ": " << p2 << std::endl;
+}
+
+std::ostream &operator<<(std::ostream &os, ft::pair<int, int> const &pair)
+{
+    os << "pair(" << pair.first << ", " << pair.second << ")";
+    return os;
+}
+
+void PairTester::test_relational_operator()
+{
+    ft::pair<int, int> p1(1, 1);
+    ft::pair<int, int> p2(2, 1);
+    ft::pair<int, int> p3(1, 2);
+
+    std::cout << std::boolalpha
+              << p1 << " == " << p1 << ": " << (p1 == p1) << std::endl
+              << p1 << " == " << p2 << ": " << (p1 == p2) << std::endl
+              << p1 << " == " << p3 << ": " << (p1 == p3) << std::endl
+              << p1 << " != " << p1 << ": " << (p1 != p1) << std::endl
+              << p1 << " != " << p2 << ": " << (p1 != p2) << std::endl
+              << p1 << " != " << p3 << ": " << (p1 != p3) << std::endl
+              << p1 << " <  " << p1 << ": " << (p1 < p1) << std::endl
+              << p1 << " <  " << p2 << ": " << (p1 < p2) << std::endl
+              << p1 << " <  " << p3 << ": " << (p1 < p3) << std::endl
+              << p2 << " <  " << p1 << ": " << (p2 < p1) << std::endl
+              << p3 << " <  " << p1 << ": " << (p3 < p1) << std::endl
+              << p1 << " <= " << p1 << ": " << (p1 <= p1) << std::endl
+              << p1 << " <= " << p2 << ": " << (p1 <= p2) << std::endl
+              << p1 << " <= " << p3 << ": " << (p1 <= p3) << std::endl
+              << p2 << " <= " << p1 << ": " << (p2 <= p1) << std::endl
+              << p3 << " <= " << p1 << ": " << (p3 <= p1) << std::endl
+              << p1 << " >  " << p1 << ": " << (p1 > p1) << std::endl
+              << p1 << " >  " << p2 << ": " << (p1 > p2) << std::endl
+              << p1 << " >  " << p3 << ": " << (p1 > p3) << std::endl
+              << p2 << " >  " << p1 << ": " << (p2 > p1) << std::endl
+              << p3 << " >  " << p1 << ": " << (p3 > p1) << std::endl
+              << p1 << " >= " << p1 << ": " << (p1 >= p1) << std::endl
+              << p1 << " >= " << p2 << ": " << (p1 >= p2) << std::endl
+              << p1 << " >= " << p3 << ": " << (p1 >= p3) << std::endl
+              << p2 << " >= " << p1 << ": " << (p2 >= p1) << std::endl
+              << p3 << " >= " << p1 << ": " << (p3 >= p1) << std::endl;
+}

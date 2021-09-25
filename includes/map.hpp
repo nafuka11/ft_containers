54c6570f63285ed667e0c19ebbf2ec7a9c96b34a
nfukada <nfukada@student.42tokyo.jp>
2021-09-25 14:53:55 +0900

    fix(map): pass value_comp to constructor of rb_tree

--- includes/map.hpp
+++ includes/map.hpp
@@ -18,24 +18,13 @@ namespace ft
         typedef pair<const key_type, mapped_type> value_type;
         typedef Compare key_compare;
         typedef Allocator allocator_type;
-        typedef typename allocator_type::reference reference;
-        typedef typename allocator_type::const_reference const_reference;
-        typedef typename allocator_type::pointer pointer;
-        typedef typename allocator_type::const_pointer const_pointer;
-        // typedef iterator;
-        // typedef const_iterator;
-        // typedef reverse_iterator<iterator> reverse_iterator;
-        // typedef reverse_iterator<const_iterator> const_reverse_iterator;
-        typedef typename allocator_type::difference_type difference_type;
-        typedef typename allocator_type::size_type size_type;
 
         class value_compare : std::binary_function<value_type, value_type, bool>
         {
-        protected:
+        public:
             Compare comp;
             value_compare(Compare c) : comp(c) {}
 
-        public:
             typedef bool result_type;
             typedef value_type first_argument_type;
             typedef value_type second_argument_type;
@@ -45,12 +34,28 @@ namespace ft
             }
         };
 
+    private:
+        typedef rb_tree_<value_type, value_compare, allocator_type> tree_type;
+
+    public:
+        typedef typename allocator_type::reference reference;
+        typedef typename allocator_type::const_reference const_reference;
+        typedef typename allocator_type::pointer pointer;
+        typedef typename allocator_type::const_pointer const_pointer;
+        typedef typename tree_type::iterator iterator;
+        typedef typename tree_type::iterator const_iterator;
+        typedef ft::reverse_iterator<iterator> reverse_iterator;
+        typedef ft::reverse_iterator<const_iterator> const_reverse_iterator;
+        typedef typename allocator_type::difference_type difference_type;
+        typedef typename allocator_type::size_type size_type;
+
     public:
         // Member functions
         // constructor
         explicit map(const key_compare &comp = key_compare(),
                      const allocator_type &alloc = allocator_type())
-            : tree_(comp, alloc) {}
+            : key_comp_(comp), value_comp_(value_compare(comp)),
+              tree_(value_comp_, alloc) {}
         // template <class InputIterator>
         // map(InputIterator first, InputIterator last,
         //     const key_compare &comp = key_compare(),
@@ -130,9 +135,9 @@ namespace ft
         }
 
     private:
-        typedef rb_tree_<value_type, key_compare, allocator_type> tree;
-
-        tree tree_;
+        key_compare key_comp_;
+        value_compare value_comp_;
+        tree_type tree_;
     };
 } /* namespace ft */
 

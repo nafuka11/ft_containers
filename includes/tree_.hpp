cccc36f6c1597c898cc7302ffcdf2e4ddd450622
nfukada <nfukada@student.42tokyo.jp>
2021-10-22 22:23:15 +0900

    revert(tree): revert to previous commit

--- includes/tree_.hpp
+++ includes/tree_.hpp
@@ -292,8 +292,7 @@ namespace ft
             if (position == end())
             {
                 iterator max = end();
-                --max;
-                if (size_ > 0 && compare_(*max, value))
+                if (size_ > 0 && compare_(*(--max), value))
                     return insert_node(value, max.base()).first;
                 else
                     return insert(value).first;

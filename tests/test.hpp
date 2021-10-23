02ea2720ee6cad66897857ff0c4339b81d3b17dd
nfukada <nfukada@student.42tokyo.jp>
2021-10-23 18:24:46 +0900

    feat(test.hpp): change order of includes

--- tests/test.hpp
+++ tests/test.hpp
@@ -8,23 +8,23 @@
 #endif
 
 #if IS_FT
-#include <map.hpp>
-// #include <stack.hpp>
 #include <vector.hpp>
+#include <map.hpp>
+#include <stack.hpp>
 #include <utility.hpp>
 // CREATE A REAL STL EXAMPLE
 #else
+#include <vector>
 #include <map>
 #include <stack>
-#include <vector>
 namespace ft = std;
 #endif
 
 #include "common/Foo.hpp"
 #include "common/MyAllocator.hpp"
 #include "common/print.hpp"
-#include "map/test_map.hpp"
 #include "vector/test_vector.hpp"
+#include "map/test_map.hpp"
 #include "stack/test_stack.hpp"
 
 #endif /* COMMON_HPP */

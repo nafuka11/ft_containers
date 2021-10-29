a1bc99c2c355d58af737ab6961b023767051a1d0
nfukada <nfukada@student.42tokyo.jp>
2021-10-29 11:19:57 +0900

    feat: add print_time()

--- tests/common/print.cpp
+++ tests/common/print.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 #include <string>
+#include <iomanip>
+#include "common/Timer.hpp"
 
 void print_test_case(const std::string &str)
 {
     std::cout << "** " << str << " **" << std::endl;
+}
+
+void print_time(const std::string &str, const Timer &timer)
+{
+    std::cout << std::setw(25) << std::left << str;
+    std::cout << timer << std::endl;
 }
